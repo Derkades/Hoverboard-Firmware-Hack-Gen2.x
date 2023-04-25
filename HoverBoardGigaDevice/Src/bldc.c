e5791768da228c29759b839bfbf2881d28e040b9
Robin Slot <robin+github@rkslot.nl>
2023-04-25 15:38:59 +0200

    Add platformio configuration

--- HoverBoardGigaDevice/Src/bldc.c
+++ HoverBoardGigaDevice/Src/bldc.c
@@ -1,7 +1,7 @@
 /*
-* This file is part of the hoverboard-firmware-hack-V2 project. The 
+* This file is part of the hoverboard-firmware-hack-V2 project. The
 * firmware is used to hack the generation 2 board of the hoverboard.
-* These new hoverboards have no mainboard anymore. They consist of 
+* These new hoverboards have no mainboard anymore. They consist of
 * two Sensorboards which have their own BLDC-Bridge per Motor and an
 * ARM Cortex-M3 processor GD32F130C8.
 *
@@ -10,8 +10,8 @@
 * Copyright (C) 2018 Kai Liebich
 * Copyright (C) 2018 Christoph Lehnert
 *
-* The program is based on the hoverboard project by Niklas Fauth. The 
-* structure was tried to be as similar as possible, so that everyone 
+* The program is based on the hoverboard project by Niklas Fauth. The
+* structure was tried to be as similar as possible, so that everyone
 * could find a better way through the code.
 *
 * This program is free software: you can redistribute it and/or modify
@@ -74,20 +74,20 @@ uint32_t speedCounter = 0;
 const uint8_t hall_to_pos[8] =
 {
 	// annotation: for example SA=0 means hall sensor pulls SA down to Ground
-  0, // hall position [-] - No function (access from 1-6) 
+  0, // hall position [-] - No function (access from 1-6)
   3, // hall position [1] (SA=1, SB=0, SC=0) -> PWM-position 3
   5, // hall position [2] (SA=0, SB=1, SC=0) -> PWM-position 5
   4, // hall position [3] (SA=1, SB=1, SC=0) -> PWM-position 4
   1, // hall position [4] (SA=0, SB=0, SC=1) -> PWM-position 1
   2, // hall position [5] (SA=1, SB=0, SC=1) -> PWM-position 2
   6, // hall position [6] (SA=0, SB=1, SC=1) -> PWM-position 6
-  0, // hall position [-] - No function (access from 1-6) 
+  0, // hall position [-] - No function (access from 1-6)
 };
 
 //----------------------------------------------------------------------------
 // Block PWM calculation based on position
 //----------------------------------------------------------------------------
-__INLINE void blockPWM(int pwm, int pwmPos, int *y, int *b, int *g)
+extern __INLINE void blockPWM(int pwm, int pwmPos, int *y, int *b, int *g)
 {
   switch(pwmPos)
 	{
@@ -152,21 +152,21 @@ void CalculateBLDC(void)
 	int y = 0;     // yellow = phase A
 	int b = 0;     // blue   = phase B
 	int g = 0;     // green  = phase C
-	
+
 	// Calibrate ADC offsets for the first 1000 cycles
   if (offsetcount < 1000)
-	{  
+	{
     offsetcount++;
     offsetdc = (adc_buffer.current_dc + offsetdc) / 2;
     return;
   }
-	
+
 	// Calculate battery voltage every 100 cycles
   if (buzzerTimer % 100 == 0)
 	{
     batteryVoltage = batteryVoltage * 0.999 + ((float)adc_buffer.v_batt * ADC_BATTERY_VOLT) * 0.001;
   }
-	
+
 #ifdef MASTER
 	// Create square wave for buzzer
   buzzerTimer++;
@@ -183,47 +183,47 @@ void CalculateBLDC(void)
 		gpio_bit_write(BUZZER_PORT, BUZZER_PIN, RESET);
   }
 #endif
-	
+
 	// Calculate current DC
 	currentDC = ABS((adc_buffer.current_dc - offsetdc) * MOTOR_AMP_CONV_DC_AMP);
 
   // Disable PWM when current limit is reached (current chopping), enable is not set or timeout is reached
 	if (currentDC > DC_CUR_LIMIT || bldc_enable == RESET || timedOut == SET)
 	{
-		timer_automatic_output_disable(TIMER_BLDC);		
+		timer_automatic_output_disable(TIMER_BLDC);
   }
 	else
 	{
 		timer_automatic_output_enable(TIMER_BLDC);
   }
-	
+
   // Read hall sensors
 	hall_a = gpio_input_bit_get(HALL_A_PORT, HALL_A_PIN);
   hall_b = gpio_input_bit_get(HALL_B_PORT, HALL_B_PIN);
 	hall_c = gpio_input_bit_get(HALL_C_PORT, HALL_C_PIN);
-  
+
 	// Determine current position based on hall sensors
   hall = hall_a * 1 + hall_b * 2 + hall_c * 4;
   pos = hall_to_pos[hall];
-	
+
 	// Calculate low-pass filter for pwm value
 	filter_reg = filter_reg - (filter_reg >> FILTER_SHIFT) + bldc_inputFilterPwm;
 	bldc_outputFilterPwm = filter_reg >> FILTER_SHIFT;
-	
+
   // Update PWM channels based on position y(ellow), b(lue), g(reen)
   blockPWM(bldc_outputFilterPwm, pos, &y, &b, &g);
-	
+
 	// Set PWM output (pwm_res/2 is the mean value, setvalue has to be between 10 and pwm_res-10)
 	timer_channel_output_pulse_value_config(TIMER_BLDC, TIMER_BLDC_CHANNEL_G, CLAMP(g + pwm_res / 2, 10, pwm_res-10));
 	timer_channel_output_pulse_value_config(TIMER_BLDC, TIMER_BLDC_CHANNEL_B, CLAMP(b + pwm_res / 2, 10, pwm_res-10));
 	timer_channel_output_pulse_value_config(TIMER_BLDC, TIMER_BLDC_CHANNEL_Y, CLAMP(y + pwm_res / 2, 10, pwm_res-10));
-	
+
 	// Increments with 62.5us
 	if(speedCounter < 4000) // No speed after 250ms
 	{
 		speedCounter++;
 	}
-	
+
 	// Every time position reaches value 1, one round is performed (rising edge)
 	if (lastPos != 1 && pos == 1)
 	{
